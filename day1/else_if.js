f65857c038057dec8c1f62bf5365da2618029d70
mohanthi4 <mohanthigajulapalli04@gmail.com>
2025-09-19 12:37:09 +0530

    i created my else if code

---
+++ day1/else_if.js
@@ -0,0 +1,13 @@
+// using else if 
+let iDrink = "butter fruit";
+if (iDrink === "amala"){
+  console.log("i drink amala juice");
+}
+else if (iDrink === "mosambi"){
+    console.log("i drink mosambi juice");
+}
+else if (iDrink === "grape"){
+    console.log("i drink grape juice");
+}else if (iDrink === "butter fruit"){
+    console.log("i drink butter fruit juice");
+}
