fe42f2af5d184d08dd5b10d3fb8090562eef967d
mohanthi4 <mohanthigajulapalli04@gmail.com>
2025-11-15 16:55:26 +0530

    create separate functions and create list

--- palindrome/is-palindrome.js
+++ palindrome/is-palindrome.js
@@ -1,13 +1,9 @@
-const value = "radha";
-const length = value.length;
-let duplicateValue = "";
-let initial = 1;
+const isReverseEqual = (string, reverse) =>
+  (string === reverse)
+    ? `${string} is a palindrome`
+    : `${string} is not palindrome`;
 
-while (initial <= length){
-  let reverseValue = value[length - initial];
-  duplicateValue = duplicateValue + reverseValue ;
-  initial = initial + 1;
-}
-
-const outcome = (value === duplicateValue) ? "is a palindrome" : "is a not palindrome";
-console.log(value,outcome);
+const isPalindrome = (string) => {
+  const reverse = string.split("").reverse().join("");
+  return isReverseEqual(string, reverse);
+};
